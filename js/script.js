25816639dc8eee5729006d851c49f16c436250f0
Chris Jones <whydoiroll@gmail.com>
2019-04-03 15:36:53 +0100

    added styles and javascript

--- js/script.js
+++ js/script.js
@@ -1,48 +1,84 @@
 /*jshint esversion: 6 */
 
-// Lightbox functionality
-// the first step we want to do is to show our lightbox when we click on an image
-// we want to make sure that the image we clicked on will be in the lightbox
-// we want to make sure that when we click on the lightbox when its visible that it will fade out and hide itself again
+// we need to listen for scroll events on the page
+// when the scroll bar reaches a certain distance
+// we want to change the background colour of the body
 
-$('.brogues img, .stationary img').on('click', function() {
-  $('.lightbox').fadeIn();
+// step1. deceide which tags we want to use and save them as contsants at the top of the page
+const bodyTag = document.querySelector("body");
 
-  let imageSrc = $(this).attr('src');
-  $('.lightbox img').attr('src', imageSrc);
-});
-
-$('.lightbox').on('click', function() {
-  $('.lightbox').fadeOut();
-});
-
-// We first need to listen for the scroll event on the page
-// Next, when our browser reaches a certain distance form the top of the page we need to change the body elements background color
-
-const body = document.querySelector("body");
-
-// Listen out for scroll events on our page
+// step 2 decide which user events ie, click, scroll etc, we want to trigger our code on. here we want to listen out for scroll events on our page
 document.addEventListener("scroll", function() {
-  //console.log("Page scrolled");
+  console.log("Page has been scrolled");
 
-  // Find out and store how far from the top of the page the scrollbar is
+  //Step 3. find out and store how far from the top of the page the scroll bar is
   let distanceFromTop = window.scrollY;
   console.log(distanceFromTop);
 
-  // If distanceFromTop is greater than 1000 pixels, do something
-  if (distanceFromTop > 1000) {
-    console.log("past 1000 pixels");
-    body.classList.add("brogue-active");
+  // if the distance from top is greater than 1000px, do something
+
+  if (distanceFromTop > 980) {
+    // console.log("passed 980px");
+    // bodyTag.style.backgroundColor = "purple";
+    bodyTag.classList.add("brogue-active");
   } else {
-    // Otherwise do something else
-    console.log("less than 1000 pixels down");
-    body.classList.remove("brogue-active");
+    bodyTag.classList.remove("brogue-active");
+    // console.log("less than 980px");
+    // bodyTag.style.backgroundColor = "#f5f5f5";
   }
 
-  if (distanceFromTop > 4400) {
-    body.classList.add("stationary-active");
+  if (distanceFromTop > 4200) {
+    bodyTag.classList.add("stationary-active");
+
   } else {
-    body.classList.remove("stationary-active");
+    bodyTag.classList.remove("stationary-active");
   }
 
 });
+
+//Lightbox
+//when we clock on an image we want to show the lightbox
+//inside the lightbox should be the mage we clicked on
+//when we click on the lightbox we want it to disappear
+
+//step 1 : decide which tags in HTML we want to use and save them in constants
+//save all of our images into a constant
+const images = document.querySelectorAll("section img");
+// save the light box from our html inside a constant
+const lightbox = document.querySelector(".lightbox");
+// save the lightbox image from our html file inside a constant
+const lightboxImage = document.querySelector(".lightbox img");
+//lets look at what saves in images
+
+images.forEach(image => {
+  // Step 2. add a click event listener to all of our images
+
+  image.addEventListener("click", function() {
+    console.log("image clicked");
+    // step 3:show the lightbox
+    lightbox.classList.add("fadeIn");
+    // step 4 show the right image
+    // save the image source attribute from the image we click on
+    let imageSrc = this.getAttribute("src");
+    console.log(imageSrc);
+    // set the lightbox image souce attribute to the one we save
+    // this means "get this"
+    lightboxImage.setAttribute("src", imageSrc);
+
+  });
+});
+
+// step 4 when we click on the lightbox remove the fadein class
+lightbox.addEventListener("click", function() {
+  lightbox.classList.remove("fadeIn");
+});
+
+// add event listener only works on singluar objects
+// bodyTag.style.backgroundColor = "purple";
+// const h1Tag = document.querySelector("h1");
+
+// document.addEventListener("scroll", function() {
+//   // bodyTag.innerHTML = "H1 has been clicked!";
+//   bodyTag.style.fontSize = "90px";
+//   bodyTag.style.backgroundColor = "#F6D2B2";
+// });
